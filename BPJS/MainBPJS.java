b8c4367c8f73a7de219cb1c55e15d4ddb9461fe3
emhabe22 <134060571+emhabe22@users.noreply.github.com>
2025-03-21 21:38:35 +0700

    Add files via upload
    BAB 1

---
+++ BPJS/MainBPJS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BPJS;
+
+/**
+ *
+ * @author HAFILLPC
+ */
+public class MainBPJS {
+    
+    
+    public static void main(String[] args) {
+        PesertaBPJS BPJS = new PesertaBPJS();
+        
+        BPJS.tampilkanInfo();
+    }
+   
+}
---
+++ BPJS/PesertaBPJS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BPJS;
+
+/**
+ *
+ * @author HAFILLPC
+ */
+public class PesertaBPJS {
+    String nama = "Hafill";
+    String NIK = "1234";
+    int nomorBPJS = 123456;
+    
+    
+    void tampilkanInfo(){
+        System.out.println("Nama :" + nama);
+        System.out.println("NIK :" + NIK);
+        System.out.println("Nomor BPJS :" + nomorBPJS);
+    }
+}
